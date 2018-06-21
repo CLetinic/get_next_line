5a886b8eeda6719c5663589d97a2c6be9ffa11fb
Chanel LETINIC <cletinic@e5r7p7.wethinkcode.co.za>
2018-06-21 12:46:09 +0200

    Does not work with multiple file descriptors, howwever basic functionality belowe the int max works. Also has one static variable. Also is normed.

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cletinic <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/08 13:21:28 by cletinic          #+#    #+#             */
+/*   Updated: 2018/06/21 10:28:57 by cletinic         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "libft/libft.h"
+# define BUFF_SIZE 32
+
+int		get_next_line(int const fd, char **line);
+
+#endif
